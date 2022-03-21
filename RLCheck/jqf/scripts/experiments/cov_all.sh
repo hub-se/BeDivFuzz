60648e858c2c08d6632ae11967ebf12c70a1061a
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2022-03-21 17:47:04 +0100

    Remove RLCheck from Repo

--- RLCheck/jqf/scripts/experiments/cov_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./cov.sh chess chess.FENTest $1
-./cov.sh ant ant.ProjectBuilderTest $1
-./cov.sh maven maven.ModelReaderTest $1
-./cov.sh bcel bcel.ParserTest $1
-./cov.sh closure closure.CompilerTest $1
-./cov.sh rhino rhino.CompilerTest $1
