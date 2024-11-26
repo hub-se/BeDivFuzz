f76494390411669c06eb801b551e8b4b60b755e7
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2024-11-27 00:49:56 +0900

    add: RLCheck implementation

--- examples/src/test/java/edu/berkeley/cs/jqf/examples/nashorn/CompilerTest.java
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/nashorn/CompilerTest.java
@@ -1,10 +1,15 @@
 package edu.berkeley.cs.jqf.examples.nashorn;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import com.pholser.junit.quickcheck.From;
 import de.hub.se.jqf.bedivfuzz.BeDivFuzz;
 import de.hub.se.jqf.bedivfuzz.examples.js.SplitJavaScriptCodeGenerator;
 import edu.berkeley.cs.jqf.examples.js.JavaScriptCodeGenerator;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assume;
 import org.junit.runner.RunWith;
 import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
@@ -34,6 +39,18 @@ public class CompilerTest {
     public void testWithSplitGenerator(@From(SplitJavaScriptCodeGenerator.class) String code) {
         //System.setProperty("nashorn.args", "--no-deprecation-warning");
         try {
+            CompiledScript compiled = ((Compilable) engine).compile(code);
+        }
+        catch (ScriptException e) {
+            Assume.assumeNoException(e);
+        }
+    }
+
+    @Fuzz
+    public void testWithInputStream(InputStream in) throws IOException {
+        //System.setProperty("nashorn.args", "--no-deprecation-warning");
+        try {
+            String code = IOUtils.toString(in, StandardCharsets.UTF_8);
             CompiledScript compiled = ((Compilable) engine).compile(code);
         }
         catch (ScriptException e) {
